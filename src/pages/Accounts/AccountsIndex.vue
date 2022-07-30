be3f5bf399684aec4cbf56a55050e8d554e5aba1
sammy-code98 <sammysmart98@yahoo.com>
2022-07-30 21:41:49 +0100

    add accout lading page

---
+++ src/pages/Accounts/AccountsIndex.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="">
+    <div
+      class="q-mt-md q-px-md text-weight-bold text-right text-font text-subtitle2"
+    >
+      or continue as
+      <span class="text-weight-bolder text-positive">Guest</span>
+    </div>
+    <div class="q-pa-md q-mt-md flex flex-center">
+      <div
+        class="text-center q-mt-md text-h3 text-weight-bold text-font text-positive"
+      >
+        Foodini
+      </div>
+      <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-vertical.svg"
+        style="width: 300px; height: 330px"
+      />
+    </div>
+    <div class="row q-ma-md q-pa-sm">
+      <q-btn
+        class="full-width q-pa-md q-ma-sm text-font text-weight-bold"
+        no-caps
+        color="positive"
+        to="/account/signup"
+        label="Sign Up"
+      />
+      <q-btn
+        class="full-width q-pa-md q-ma-sm text-font text-weight-bold"
+        no-caps
+        outline
+        color="positive"
+        to="/account/signin"
+        label="Sign In"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { useMeta } from "quasar";
+const metaData = {
+  title: "Medicals ",
+};
+export default {
+  name: "PageIndex",
+  setup() {
+    useMeta(metaData);
+  },
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,14 @@ const routes = [
     children: [{ path: "", component: () => import("pages/Index.vue") }],
   },
 
+  {
+    path: "/accounts",
+    component: () => import("layouts/AcctLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/Accounts/AccountsIndex.vue") },
+    ],
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
