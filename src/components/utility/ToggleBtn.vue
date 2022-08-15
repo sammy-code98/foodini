4f9001fb770045d130d70816b15a13a7030db92e
sammy-code98 <sammysmart98@yahoo.com>
2022-08-15 15:50:39 +0100

    update profile ui

--- src/components/utility/ToggleBtn.vue
+++ src/components/utility/ToggleBtn.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="rounded bg-indigo-1">
-    <q-btn
-      no-caps
-      flat
-      @click="toggle()"
-      :class="[toggleMode ? 'active' : 'bg-indigo-1']"
-      class="rounded"
-      >Light Mode</q-btn
-    >
-    <q-btn
-      no-caps
-      flat
-      @click="toggle()"
-      :class="[!toggleMode ? 'active' : 'bg-indigo-1']"
-      class="rounded"
-      >Dark Mode</q-btn
-    >
+    <q-btn-group spread class="rounded no-shadow">
+      <q-btn
+        no-caps
+        flat
+        size="16px"
+        @click="toggle()"
+        :class="[toggleMode ? 'active' : 'bg-indigo-1']"
+        class="rounded"
+        >Light Mode</q-btn
+      >
+      <q-btn
+        no-caps
+        flat
+        size="16px"
+        @click="toggle()"
+        :class="[!toggleMode ? 'active' : 'bg-indigo-1']"
+        class="rounded"
+        >Dark Mode</q-btn
+      >
+    </q-btn-group>
   </div>
 </template>
 
--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -1,20 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <div>
-      <q-avatar class="rounded" size="200px">
-        <img src="https://cdn.quasar.dev/img/avatar.png" />
-      </q-avatar>
-      <div class="q-mt-md text-center text-weight-bold">
-        <div class="text-h5 text-grey-8">Olake Folake</div>
-        <div class="text-subtitle1 text-grey-7">+234 0002223223</div>
-        <div class="text-positive">Edit Profile</div>
-      </div>
-
-      <div class="q-mt-md q-gutter-y-md rounded">
-        <toggle-btn />
-      </div>
+  <div class="q-pa-md flex justify-center items-center q-mt-xl">
+    <q-avatar class="rounded" size="200px">
+      <img src="https://cdn.quasar.dev/img/avatar.png" />
+    </q-avatar>
+    <div class="q-mt-md text-center text-weight-bold">
+      <div class="text-h5 text-grey-8">Olake Folake</div>
+      <div class="text-subtitle1 text-grey-7">+234 0002223223</div>
+      <div class="text-positive">Edit Profile</div>
     </div>
-  </q-page>
+  </div>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md rounded">
+      <toggle-btn />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,3 +29,14 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.center {
+  position: absolute;
+  margin: 20px auto;
+  top: 50%;
+  transform: translate(0, 50%);
+  border: 5px solid #ffff00;
+  padding: 10px;
+}
+</style>
