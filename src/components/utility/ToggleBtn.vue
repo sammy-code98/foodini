2732a45928af9871c5865e1b710771605765df80
sammy-code98 <sammysmart98@yahoo.com>
2022-08-15 15:08:28 +0100

    add toogle button

---
+++ src/components/utility/ToggleBtn.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="rounded bg-indigo-1">
+    <q-btn
+      no-caps
+      flat
+      @click="toggle()"
+      :class="[toggleMode ? 'active' : 'bg-indigo-1']"
+      class="rounded"
+      >Light Mode</q-btn
+    >
+    <q-btn
+      no-caps
+      flat
+      @click="toggle()"
+      :class="[!toggleMode ? 'active' : 'bg-indigo-1']"
+      class="rounded"
+      >Dark Mode</q-btn
+    >
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const toggleMode = ref(true);
+    const toggle = () => {
+      toggleMode.value = !toggleMode.value;
+      console.log("keke");
+    };
+    return {
+      toggleMode,
+      toggle,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.active {
+  color: $positive;
+  background: #fff;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+}
+</style>
---
+++ src/pages/UserProfile.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <q-avatar class="rounded" size="200px">
+        <img src="https://cdn.quasar.dev/img/avatar.png" />
+      </q-avatar>
+      <div class="q-mt-md text-center text-weight-bold">
+        <div class="text-h5 text-grey-8">Olake Folake</div>
+        <div class="text-subtitle1 text-grey-7">+234 0002223223</div>
+        <div class="text-positive">Edit Profile</div>
+      </div>
+
+      <div class="q-mt-md q-gutter-y-md rounded">
+        <toggle-btn />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import ToggleBtn from "../components/utility/ToggleBtn.vue";
+
+export default {
+  components: {
+    ToggleBtn,
+  },
+
+  setup() {
+    return {};
+  },
+};
+</script>
